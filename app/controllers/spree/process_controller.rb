17e423a011afc613cfc945b1afaa766569441a6e
Yosep Kim <yosepk@gmail.com>
2013-09-05 03:08:36 -0400

    initial setup

---
+++ app/controllers/spree/process_controller.rb
@@ -0,0 +1,51 @@
+module Spree
+	class ProcessController < Spree::StoreController
+		helper 'spree/products'
+    respond_to :html
+    
+		def step1
+			#@searcher = build_searcher(params)
+      #@products = @searcher.retrieve_products
+      
+      # get all OS types
+      @type = Spree::OptionType.where(:name => 'Operating System').first
+      if @type
+      	@options = @type.option_values
+      else
+      	@options = []
+      end
+		end
+
+		def step2
+			session[:selected_os] = params[:selected_value] if ! params[:selected_value].blank?
+			@type = Spree::OptionType.where(:name => 'CPU').first
+      if @type
+      	@options = @type.option_values
+      else
+      	@options = []
+      end
+		end
+
+		def step3
+			session[:selected_cpu] = params[:selected_value] if ! params[:selected_value].blank?
+			@type = Spree::OptionType.where(:name => 'Memory').first
+      if @type
+      	@options = @type.option_values
+      else
+      	@options = []
+      end
+		end
+		
+		def step4
+			session[:selected_memory] = params[:selected_value] if ! params[:selected_value].blank?
+			
+			os = session[:selected_os]
+			cpu = session[:selected_cpu]
+			memory = session[:selected_memory]
+			
+			@variant = Spree::Variant.joins(:option_values).where("spree_option_values.id in (?)", [os, cpu, memory])
+				.group('spree_variants.id').having('count(*) = 3').first
+			@product = @variant.product
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = 'b47cbc6ee68e379fc0a475e27609e0f3780ba9f1fd40ea5218ab9b0441e67c5965d2fac9621b58de3f9f76c574f4a2350a9f7db67e1b7e849df17f661cf8d4a6'
